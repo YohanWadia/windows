609afef554132bd229eafcdd755e916e66e87678
Yohan Wadia <persianpowerman@hotmail.com>
2019-10-24 10:22:28 +0300

    NEW NEW NEW

--- src/WinFeatures.java
+++ src/WinFeatures.java
@@ -1,15 +1,3 @@
 public class WinFeatures {
-    int win7x;
 
-    public void winUpdate(){
-        //this is our 1st update
-    }
-
-    public void win7_Patch(){
-        //saved win7!!
-    }
-
-    public void superUpdate(){
-        //even better...
-    }
 }
